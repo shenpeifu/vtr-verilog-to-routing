dc082a98d8df16253e61429ff7080e3d0b54971c
jrudolph.at.work@gmail.com <jrudolph.at.work@gmail.com@8e3573b8-cf2c-4f14-ef6d-137439e28b8b>
2012-03-14 17:37:28 +0000

    Code review branch for clock resets...

---
+++ branches/vpr_TI_global_resets/vpr/SRC/pack/output_clustering.h
@@ -0,0 +1,9 @@
+void output_clustering(	t_block *clb,
+						int num_clusters,
+						boolean global_clocks,
+						boolean * is_clock,
+/* JR-022412 */
+						boolean * is_reset,
+						char *out_fname,
+						boolean skip_clustering);
+
