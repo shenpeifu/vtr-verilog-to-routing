e5bb52878689469465fe5f02e2160a1a30f25f1a
andrehpereira@gmail.com <andrehpereira@gmail.com@8e3573b8-cf2c-4f14-ef6d-137439e28b8b>
2013-05-29 20:29:24 +0000

    Changed the code to create extra switches with increased delay

--- branches/pereira/vpr/SRC/base/globals.c
+++ branches/pereira/vpr/SRC/base/globals.c
@@ -102,3 +102,6 @@ t_clock_arch * g_clock_arch;
 int percent_wires_cut = 0;
 int num_cuts = 0;
 int delay_increase = 0;
+
+/* Mapping from edges to edges with increased delay */
+int *increased_delay_edge_map = NULL;
