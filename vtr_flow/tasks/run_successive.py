854bdaf8cb48007a75de4aa085ca413a24bfcffa
andrehpereira <andrehpereira@gmail.com>
2013-06-06 21:45:28 +0000

    Added script to help running tasks

---
+++ vtr_flow/tasks/run_successive.py
@@ -0,0 +1,20 @@
+import os
+
+def main():
+    vals = [0, 10, 20, 30, 40, 50, 60, 70, 80, 85, 90]
+    init = int(raw_input())
+    i = 0
+    for j in xrange(len(vals)):
+        if(vals[j] >= init):
+            i = j
+            break
+
+    for p in xrange(i, len(vals)):
+        print "Running with percent_wires_cut=%d" % (vals[p])
+        os.system("../scripts/run_vtr_task.pl timing_small -p 3 -percent_wires_cut %d -num_cuts 2 -delay_increase 1000" % (vals[p]))
+        os.system("../scripts/parse_vtr_task.pl timing_small")
+        print "Finished running with percent_wires_cut=%d" % (vals[p])
+
+
+
+main()
