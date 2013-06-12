8d65050ba97fc80509699df58721a85fc4ad9555
andrehpereira@gmail.com <andrehpereira@gmail.com@8e3573b8-cf2c-4f14-ef6d-137439e28b8b>
2013-06-12 19:48:59 +0000

    Added recognition for cutline delays at placer

--- branches/pereira/vtr_flow/tasks/run_successive.py
+++ branches/pereira/vtr_flow/tasks/run_successive.py
@@ -1,7 +1,8 @@
 import os
 
 def main():
-    vals = [0, 10, 20, 30, 40, 50, 60, 70, 80, 85, 90]
+    #vals = [0, 10, 20, 30, 40, 50, 60, 70, 80, 85, 90]
+    vals = [0, 20, 40, 60, 80]
     print "Enter initial value for percent_wires_cut"
     init = int(raw_input())
     i = 0
@@ -12,7 +13,7 @@ def main():
 
     for p in xrange(i, len(vals)):
         print "Running with percent_wires_cut=%d" % (vals[p])
-        os.system("../scripts/run_vtr_task.pl timing_small -p 4 -percent_wires_cut %d -num_cuts 1 -delay_increase 1000" % (vals[p]))
+        os.system("../scripts/run_vtr_task.pl timing_small -p 5 -percent_wires_cut %d -num_cuts 3 -delay_increase 1000" % (vals[p]))
         os.system("../scripts/parse_vtr_task.pl timing_small")
         print "Finished running with percent_wires_cut=%d" % (vals[p])
 
