319a3cbebab48327bfa710b43e422ec02ae95630
o.d.petelin <o.d.petelin@8e3573b8-cf2c-4f14-ef6d-137439e28b8b>
2013-08-14 23:52:59 +0000

    Adding functions for reading-in switchblock permutation funcs and wire type/group connections

--- branches/opetelin/libarchfpga/include/parse_switchblocks.h
+++ branches/opetelin/libarchfpga/include/parse_switchblocks.h
@@ -1,6 +1,9 @@
 #ifndef XML_ARCH_SWITCHBLOCKS_H
 #define XML_ARCH_SWITCHBLOCKS_H
 
+/**** Includes ****/
+#include "ezxml.h"
+
 /**** Defines ****/
 
 /**** Structs ****/
@@ -19,6 +22,12 @@ struct s_formula_data{
 /**** Classes ****/
 
 /**** Function Declarations ****/
+/* Loads permutation funcs specified under Node into t_switchblock_inf */
+void read_sb_switchfuncs( INP ezxml_t Node, INOUTP t_switchblock_inf *sb );
+
+/* Reads-in the wire connections specified for the switchblock in the xml arch file */
+void read_sb_wireconns( INP ezxml_t Node, INOUTP t_switchblock_inf *sb );
+
 /* returns integer result according to the specified formula and data */
 int get_sb_formula_result( INP const char* formula, INP const s_formula_data &mydata );
 
